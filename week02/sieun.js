5268364134c1caccc94a260fc47545234ca17ec3
c-eun-5 <sieunoh24@sookmyung.ac.kr>
2024-03-24 13:36:17 +0900

    js추가

---
+++ week02/sieun.js
@@ -0,0 +1,11 @@
+alert("Welcomw to Sieun info page!");
+const mainbox = document.getElementById("mainbox");
+function darkmode() {
+    mainbox.style.backgroundColor = '#262626';
+    mainbox.style.color = 'White';
+}
+function lightmode() {
+    mainbox.style.backgroundColor = 'white';
+    mainbox.style.color = 'black';
+    h2.style.backgroundColor - 'cornsilk';
+}
